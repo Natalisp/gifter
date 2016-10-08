4c57ebf329202b680c91fcbcce48e973ae63aa63
Natalisp <nathalipat@gmail.com>
2016-10-08 19:23:18 -0400

    made frienships controller and views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,26 @@
 class FriendshipsController < ApplicationController
+
+  def index
+    @users = Users.all
+  end
+
+  def show
+  end
+
+  def create
+    @friendship = Friendship.new(friendship_params)
+    friendship.save
+    redirect_to root_url
+  end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+    redirect_to root_url
+  end
+
+  private
+  def friendship_params
+    params.require(:friendship)
+  end
 end
