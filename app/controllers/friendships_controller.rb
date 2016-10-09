f04561aed4d184bc7887bade20a3f36c39dafd54
Natalisp <nathalipat@gmail.com>
2016-10-08 21:42:24 -0400

    friendship made mutual

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,13 +9,17 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    friend = User.find_by(id: params[:friend_id])
+    friend.friendships.build(friend_id: current_user.id)
+    friend.save
     @friendship.save
     redirect_to user_url(User.find_by(id: params[:friend_id]))
   end
 
   def destroy
-    @friendship = Friendship.find(params[:id])
+    @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
+    #   REDIRECT to FRIENDS list!
     redirect_to root_url
   end
 
