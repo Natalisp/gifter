370e7cc481f2010e1855acf064f799a1daca1cd8
Natalisp <nathalipat@gmail.com>
2016-10-12 15:16:44 -0400

    profile page done, displaying my purchases

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    if current_user
+      @purchases = current_user.purchases
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, only: [:index, :show]
 
-  resources :users, only: [:show] do
+  resources :users do
     resources :wishlists
   end
 
